afb9d2e02298f4e24541b6f71848ad4129ca79f4
Özgür Öztürk <56519674+BestInSpire@users.noreply.github.com>
2024-11-12 10:33:31 +0300

    Update activation_functions.cpp

--- src/activation_functions.cpp
+++ src/activation_functions.cpp
@@ -1,10 +1,10 @@
 #include "../include/activation_functions.h";
 using namespace Eigen;
 
-MatrixXd sigmoid(constexpr MatrixXd &x) {
+MatrixXd sigmoid(const MatrixXd &x) {
     return 1.0 / (1.0 + (-x.array()).exp());
 }
-MatrixXd tanh(constexpr MatrixXd &x) {
+MatrixXd tanh(const MatrixXd &x) {
     return x.array().tanh();
 }
 
